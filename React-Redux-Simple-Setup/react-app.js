789ab8f82f1bd91eda877815929135165a8c9d11
David <dhahn123@gmail.com>
2019-04-08 14:52:19 -0700

    MapStateToProps, MapDispatchToProps, Connect Redux to React

--- React-Redux-Simple-Setup/react-app.js
+++ React-Redux-Simple-Setup/react-app.js
@@ -35,6 +35,30 @@ class DisplayMessages extends React.Component {
       );
     }
   };
+/*
+The Provider component allows you to provide state and dispatch to your React components, but you must specify exactly what state and actions you want. This way, you make sure that each component only has access to the state it needs. You accomplish this by creating two functions: mapStateToProps() and mapDispatchToProps().
+
+In these functions, you declare what pieces of state you want to have access to and which action creators you need to be able to dispatch. Once these functions are in place, you'll see how to use the React Redux connect method to connect them to your components in another challenge.
+
+Note: Behind the scenes, React Redux uses the store.subscribe() method to implement mapStateToProps().
+*/
+const mapStateToProps = (state) => {
+    return { messages: state }
+  };
+/*
+The mapDispatchToProps() function is used to provide specific action creators to your React components so they can dispatch actions against the Redux store. It's similar in structure to the mapStateToProps() function you wrote in the last challenge. It returns an object that maps dispatch actions to property names, which become component props. However, instead of returning a piece of state, each property returns a function that calls dispatch with an action creator and any relevant action data. You have access to this dispatch because it's passed in to mapDispatchToProps() as a parameter when you define the function, just like you passed state to mapStateToProps(). Behind the scenes, React Redux is using Redux's store.dispatch() to conduct these dispatches with mapDispatchToProps(). This is similar to how it uses store.subscribe() for components that are mapped to state. 
+*/
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      submitNewMessage: (newMessage) => {
+         dispatch(addMessage(newMessage))
+      }
+    }
+  };
+
+  const connect = ReactRedux.connect;
+  // define the Container component here:
+  const Container = connect(mapStateToProps,mapDispatchToProps)(Presentational);
 /* 
 React Redux provides its react-redux package to help accomplish these tasks.
 
